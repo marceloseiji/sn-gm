6eb2d9efe20bfcae62a1bae84a53b31c5b6f97b7
marceloseiji <marcelo.kihon@gmail.com>
2020-05-16 15:36:43 -0300

    js complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ let secondary = '#FFCE00';
 
 const grid = 32;
 let count = 0;
+let score = 0;
 
 let snake = {
   x: grid * 5,
@@ -44,17 +45,68 @@ function Update() {
     snake.x = 0;
   }
 
+  if(snake.y < 0) {
+    snake.y = canvas.height - grid;
+  } else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+
   snake.cells.unshift({x: snake.x, y: snake.y});
 
   if(snake.cells.length > snake.maxCells) {
     snake.cells.pop();
   }
 
+  //Draw Apple
+  ctx.fillStyle = secondary;
+  ctx.fillRect(apple.x, apple.y, grid-1, grid-1);
+
+  //Draw Snake
   ctx.fillStyle = primary;
   snake.cells.forEach(function(cell, index) {
     ctx.fillRect(cell.x, cell.y, grid-1, grid-1);
+
+    if(cell.x === apple.x && cell.y === apple.y) {
+      snake.maxCells++;
+      score++;
+
+      apple.x = getRandomInt(0, 24) * grid;
+      apple.y = getRandomInt(0, 14) * grid;
+    }
+
+    for(let i = index + 1; i < snake.cells.length; i++) {
+      if(cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        window.location.reload();
+      }
+    }
   });
+
+  //Draw score
+  ctx.font = "72px Helvetica";
+  ctx.fillStyle = "rgba(100, 100, 100, 0.5)";
+  ctx.textAlign = "center";
+  ctx.fillText(score, canvas.width / 2, canvas.height / 2);
 }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+document.addEventListener("keydown", function(evt) {
+  if(evt.which === 37 && snake.vx === 0) {
+    snake.vx = -grid;
+    snake.vy = 0;
+  } else if(evt.which === 38 && snake.vy === 0) {
+    snake.vy = -grid;
+    snake.vx = 0;
+  } else if(evt.which === 39 && snake.vx === 0) {
+    snake.vx = grid;
+    snake.vy = 0;
+  } else if(evt.which === 40 && snake.vy === 0) {
+    snake.vy = grid;
+    snake.vx = 0;
+  }
+});
 
 //Start the game
 requestAnimationFrame(Update);
